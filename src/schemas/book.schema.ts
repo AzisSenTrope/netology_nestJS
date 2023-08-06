4dd1f7f874a21082fdf68c40e737f06f827cb247
Belykh Yaroslav <orinnov.orin@yandex.ru>
2023-08-06 13:20:25 +0300

    Pochti

---
+++ src/schemas/book.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type BookDocument = Book & Document;
+
+@Schema()
+export class Book {
+  @Prop({ required: true })
+  public id: string;
+
+  @Prop({ required: true })
+  public title: string;
+
+  @Prop({ required: true })
+  public authors: string;
+
+  @Prop({ required: true })
+  public description: string;
+
+  @Prop({ required: true })
+  public favorite: string;
+
+  @Prop({ required: true })
+  public fileCover: string;
+
+  @Prop({ required: true })
+  public fileName: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
